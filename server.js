f85198bcbcb0adea7cb1d3001be7b008767180c7
Oleg Astashin <95183292+ophlop@users.noreply.github.com>
2026-05-27 17:18:32 +0400

    feat: add POST /api/check_answer route & little refactoring

--- server.js
+++ server.js
@@ -2,50 +2,81 @@ const http = require("http");
 const fs = require('fs');
 const path = require('path');
 
-// const
 const PORT = 8000;
 
 const app = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "text/html; charset=utf-8;");
-
-    // Обработка статики (CSS, JS, изображения)
-    if (req.url.startsWith('/static/')) {
-        const filePath = path.join(__dirname, req.url);
-        const ext = path.extname(filePath);
-        const contentType = ext === '.css' ? 'text/css' : 'text/plain';
-        
-        res.setHeader('Content-Type', contentType);
-        fs.readFile(filePath, (err, content) => {
-            if (err) { res.writeHead(404); res.end(); return; }
-            res.writeHead(200);
-            res.end(content);
-        });
-        return;
-    }
-
-    if (req.url === '/' || req.url === '/home') {
-        const indexPage = path.join(__dirname, 'static', 'index.html');
-
-        fs.readFile(indexPage, (err, content) => {
-            if (err) {
-                res.writeHead(404);
-                res.end('File not found');
-                return;
-            }
-
-            res.writeHead(200);
-            res.end(content);
-        })
-
-    }
-
-    if (req.url.includes('api')) {
-        if (req.url.includes('random_question')) {
-            console.log('random_question');
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.write(JSON.stringify({id: 1, question: 'How we can start work with git in project?'}));
-            res.end();
+    if (req.method === 'GET') {
+        if (req.url.startsWith('/static/')) {
+            const filePath = path.join(__dirname, req.url);
+            const ext = path.extname(filePath);
+            const contentType = ext === '.css' ? 'text/css' : 'text/plain';
+            
+            res.setHeader('Content-Type', contentType);
+            fs.readFile(filePath, (err, content) => {
+                if (err) { res.writeHead(404); res.end(); return; }
+                res.writeHead(200);
+                res.end(content);
+            });
+            return;
         }
+
+        if (req.url === '/' || req.url === '/home') {
+            const indexPage = path.join(__dirname, 'static', 'index.html');
+
+            fs.readFile(indexPage, (err, content) => {
+                if (err) {
+                    res.writeHead(404);
+                    res.end('File not found');
+                    return;
+                }
+
+                res.writeHead(200);
+                res.end(content);
+            })
+        }
+    }
+
+    if (req.url.includes('/api/')) {
+        if (req.method === 'POST') {
+            let body = '';
+            let parsedData;
+
+            req.on('data', function (data) {
+                body += data.toString();
+            });
+
+            req.on('end', function () {
+                parsedData = JSON.parse(body);
+
+                console.log(parsedData);
+
+                if (req.url.includes('check_answer')) {
+                    let answer = {};
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    if (parsedData.answer == 'git init') {
+                        answer.status = 'Good';
+                        answer.text = 'Right answer!'; 
+                    } else {
+                        answer.status = 'Bad'
+                        answer.text = 'Wrong answer!'; 
+                    }
+
+                    res.write(JSON.stringify(answer));
+                    res.end();
+                }
+            });
+
+
+        }
+
+        if (req.method === 'GET') {
+            if (req.url.includes('random_question')) {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.write(JSON.stringify({id: 1, question: 'How we can start work with git in project?'}));
+                res.end();
+            }
+        }
+
     }
 })
 
