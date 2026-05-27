b2973b7eba74df1ea2d692e7918bd5bf50a5e546
Oleg Astashin <95183292+ophlop@users.noreply.github.com>
2026-05-27 17:20:45 +0400

    feat: added fetch /api/check_answer & little refactor

--- static/script.js
+++ static/script.js
@@ -1,6 +1,8 @@
-const startButton = document.querySelector('.button');
+const startButton = document.querySelector('#start_btn');
+const answerButton = document.querySelector('#answer_btn');
 const queSection = document.querySelector('#question_section');
-const questionParagraph =  document.querySelector('.question');
+const questionParagraph = document.querySelector('.question');
+const answerInput = document.querySelector('#answer_input');
 
 startButton.addEventListener('click', () => {
     (async () => {
@@ -8,10 +10,41 @@ startButton.addEventListener('click', () => {
             const res = await fetch('/api/random_question')
             const data = await res.json();
                 
-            questionParagraph.textContent = data.question;
+            questionParagraph.textContent = 'q: ' + data.question;
+
+            startButton.hidden = true;
 
             queSection.classList.remove('question_section_none');
             queSection.classList.add('question_section');
+        } catch (err) {
+            console.error('Failed to load question:', err);
+        }
+    })();
+})
+
+answerButton.addEventListener('click', () => {
+    (async () => {
+        try {
+            const reqObj = {};
+
+            if (answerInput.value !== "") {
+                reqObj.answer = answerInput.value;
+
+                console.log(reqObj);
+
+                const res = await fetch('/api/check_answer', {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(reqObj)
+                })
+
+                const data = await res.json();
+
+                console.log(data);
+            }
+
         } catch (err) {
             console.error('Failed to load question:', err);
         }
