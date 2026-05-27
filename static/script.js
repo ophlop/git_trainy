bc953e2d9e53eb6254c6e4c9325f341fa34e8688
Oleg Astashin <95183292+ophlop@users.noreply.github.com>
2026-05-27 18:11:47 +0400

    refactor: removed iife function calls

--- static/script.js
+++ static/script.js
@@ -4,49 +4,46 @@ const queSection = document.querySelector('#question_section');
 const questionParagraph = document.querySelector('.question');
 const answerInput = document.querySelector('#answer_input');
 
-startButton.addEventListener('click', () => {
-    (async () => {
-        try {
-            const res = await fetch('/api/random_question')
+startButton.addEventListener('click', async () => {
+    try {
+        const res = await fetch('/api/random_question')
+        const data = await res.json();
+            
+        questionParagraph.textContent = 'q: ' + data.question;
+
+        startButton.hidden = true;
+
+        queSection.classList.remove('question_section_none');
+        queSection.classList.add('question_section');
+    } catch (err) {
+        console.error('Failed to load question:', err);
+    }
+});
+
+
+answerButton.addEventListener('click', async () => {
+    try {
+        const reqObj = {};
+
+        if (answerInput.value.trim() !== "") {
+            reqObj.answer = answerInput.value;
+
+            console.log(reqObj);
+
+            const res = await fetch('/api/check_answer', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(reqObj)
+            })
+
             const data = await res.json();
-                
-            questionParagraph.textContent = 'q: ' + data.question;
 
-            startButton.hidden = true;
-
-            queSection.classList.remove('question_section_none');
-            queSection.classList.add('question_section');
-        } catch (err) {
-            console.error('Failed to load question:', err);
+            console.log(data);
         }
-    })();
-})
 
-answerButton.addEventListener('click', () => {
-    (async () => {
-        try {
-            const reqObj = {};
-
-            if (answerInput.value !== "") {
-                reqObj.answer = answerInput.value;
-
-                console.log(reqObj);
-
-                const res = await fetch('/api/check_answer', {
-                    method: 'POST',
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(reqObj)
-                })
-
-                const data = await res.json();
-
-                console.log(data);
-            }
-
-        } catch (err) {
-            console.error('Failed to load question:', err);
-        }
-    })();
-})
+    } catch (err) {
+        console.error('Failed to load question:', err);
+    }
+});
