857622bfdb472e7fdb88c6e422ee7cbd0baed35d
Oleg Astashin <95183292+ophlop@users.noreply.github.com>
2026-05-28 18:04:47 +0400

    feat: add logic for quiz

--- static/script.js
+++ static/script.js
@@ -1,25 +1,20 @@
 const startButton = document.querySelector('#start_btn');
+const nextButton = document.querySelector('#next_btn');
 const answerButton = document.querySelector('#answer_btn');
+
 const queSection = document.querySelector('#question_section');
+
 const questionParagraph = document.querySelector('.question');
+const answerParagraph = document.querySelector('.answer');
 const answerInput = document.querySelector('#answer_input');
 
-startButton.addEventListener('click', async () => {
-    try {
-        const res = await fetch('/api/random_question')
-        const data = await res.json();
-            
-        questionParagraph.textContent = 'q: ' + data.question;
 
-        startButton.hidden = true;
+answerParagraph.hidden = true;
+nextButton.hidden = true;
+let currentQuestion = null;
 
-        queSection.classList.remove('question_section_none');
-        queSection.classList.add('question_section');
-    } catch (err) {
-        console.error('Failed to load question:', err);
-    }
-});
-
+startButton.addEventListener('click', getRandomQuestion);
+nextButton.addEventListener('click', getRandomQuestion);
 
 answerButton.addEventListener('click', async () => {
     try {
@@ -27,6 +22,10 @@ answerButton.addEventListener('click', async () => {
 
         if (answerInput.value.trim() !== "") {
             reqObj.answer = answerInput.value;
+            reqObj.id = currentQuestion.id
+            answerInput.disabled = true;
+            answerButton.disabled = true;
+            answerButton.hidden = true;
 
             console.log(reqObj);
 
@@ -40,10 +39,41 @@ answerButton.addEventListener('click', async () => {
 
             const data = await res.json();
 
-            console.log(data);
+            answerParagraph.textContent = data.text;
+            answerParagraph.classList.add(data.status);
+            answerParagraph.hidden = false;
+
+            nextButton.hidden = false;
         }
 
     } catch (err) {
         console.error('Failed to load question:', err);
     }
 });
+
+async function getRandomQuestion () {
+    try {
+        startButton.hidden = true;
+        nextButton.hidden = true;
+        questionParagraph.hidden = true;
+        answerParagraph.hidden = true;
+
+        answerInput.value = '';
+        answerInput.disabled = false;
+
+        const res = await fetch('/api/random_question')
+        currentQuestion = await res.json();
+            
+        questionParagraph.textContent = 'q: ' + currentQuestion.question;
+
+        questionParagraph.hidden = false;
+        answerButton.disabled = false;
+        answerButton.hidden = false;
+        
+
+        queSection.classList.remove('question_section_none');
+        queSection.classList.add('question_section');
+    } catch (err) {
+        console.error('Failed to load question:', err);
+    }
+}
